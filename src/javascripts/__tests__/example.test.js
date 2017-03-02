9d025435e6e5ca2b1f68496eaadeff4ca8694b00
Charles Loflin <cloflin@bnoinc.com>
2017-03-02 14:51:06 -0500

    initial commit

---
+++ src/javascripts/__tests__/example.test.js
@@ -0,0 +1,13 @@
+// See http://chaijs.com/ for assertion style docs
+
+describe('something', () => {
+  it('does some stuff', () => {
+    assert.isTrue(true)
+  })
+})
+
+describe('Config Tests', () => {
+  it('passes this canary test', () => {
+    assert.isTrue(true);
+  });
+});
